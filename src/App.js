5d7acc17503b67e28fdc995256e4f37713679681
Fermin Aguilar <ferminab99@gmail.com>
2021-12-04 13:14:26 -0300

    se agrego el sistema de rutas y el navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navigation from './components/common/Navigation';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+    <Navigation/>
+    <Routes>
+      <Route exact path="/seccion/actualidad" ></Route>
+      <Route exact path="/seccion/espectaculos" ></Route>
+      <Route exact path="/seccion/tecnologia" ></Route>
+      <Route exact path="/seccion/deportes" ></Route>
+      <Route exact path="/seccion/politica" ></Route>
+      <Route exact path="/seccion/economia" ></Route>
+      <Route exact path="/seccion/salud" ></Route>
+      <Route exact path="/seccion/fotografias" ></Route>
+      <Route exact path="/servicio/clima" ></Route>
+      <Route exact path="/servicio/moneda" ></Route>
+      <Route exact path="/institucional/contacto" ></Route>
+      <Route exact path="/institucional/acerca-de" ></Route>
+      <Route exact path="/institucional/publicidad" ></Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
