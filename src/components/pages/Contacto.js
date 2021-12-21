a5a0b375dcd9d82eaf29e3e85d23189a96f96a64
aimecitar <maimesaracho@gmail.com>
2021-12-20 23:43:49 -0300

    maquetado de página de contacto

--- src/components/pages/Contacto.js
+++ src/components/pages/Contacto.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
+import { Form, Button} from "react-bootstrap";
 
 const Contacto = () => {
-    return (
-        <div>
-            <h1 className="fs-1">PAGINA DE CONTACTO</h1>
-        </div>
-    );
+  return (
+    <div>
+      
+      <Form className='container my-5 mb-4 col-md-6'>
+      <br/>
+
+        <h1 className='d-flex align-items-center'>CONTÁCTENOS</h1>
+        <Form.Group className=' flex-column align-items'>
+          <Form.Label>Nombre completo</Form.Label>
+          <Form.Control type='text'/>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Mensaje</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+
+        <Button variant="danger" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 };
 
-export default Contacto
+export default Contacto;
