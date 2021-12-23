b5cf11f9eb29187468864c915997e5ceea3707fb
Charly <carlosjoaquincarral@gmail.com>
2021-12-23 15:02:13 -0300

    se traen cambios

--- src/components/pages/Error404.js
+++ src/components/pages/Error404.js
@@ -1,24 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
 import Error from '../../img/Error.png';
 
 const Error404 = () => {
     return (
         <div className=" mt-5 bg-dark text-center">
             <img src={Error} alt="logo de error"/> 
-=======
-
-const Error404 = () => {
-    return (
-        <div>
-            <h1 className="m-5 p-5">ERROR 404</h1>
->>>>>>> ddb3426f583e92126151000cba3017f0344bf0e1
         </div>
     );
 };
 
-<<<<<<< HEAD
 export default Error404;
-=======
-export default Error404
->>>>>>> ddb3426f583e92126151000cba3017f0344bf0e1
